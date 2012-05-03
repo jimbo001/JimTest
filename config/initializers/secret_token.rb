001f59f00822723ec9119662e6b52bf120b50ca6
Jim Kenyon <kenyon_jim@hotmail.com>
2012-05-03 10:54:59 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JimTest::Application.config.secret_token = 'c20267e58d713df1758427e4cbc318b3093ae67bf76546f5373b520a51c11662c6e201cd05cc65998af7b33f20efe7573e0ad7e026d5e685b2fedc7d6716bb1c'
